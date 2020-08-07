5c6b8d098ccde3c3ec2385111c71174f89380bcb
shrangi <ss.darksiders@gmail.com>
2020-08-07 14:37:03 +0530

    updated functions to run the game

--- EightQueens_Question/src/ChessBoardRenderer.java
+++ EightQueens_Question/src/ChessBoardRenderer.java
@@ -2,7 +2,14 @@ public class ChessBoardRenderer {
 	public boolean isBlackSquare(int square) {
 
 		// WRITE YOUR LOGIC HERE...
-		System.out.println(square);
-		return true;
+		int ser = square/8;
+		if(ser%2==0) {
+			if(square%2==1) return true;
+			else return false;
+		}else {
+			if(square%2==0) return true;
+			else return false;
+		}
+		
 	}
 }
--- EightQueens_Question/src/GameStrategy.java
+++ EightQueens_Question/src/GameStrategy.java
@@ -4,13 +4,29 @@ public class GameStrategy {
 
 	private int getColumn(int cellId) {
 		// WRITE YOUR LOGIC HERE...................................		
-
+		int counter=0;
+		for(int i=0;i<8;i++) {
+			for(int k=0;k<8;k++) {
+				if(counter==cellId) {
+					return k;
+				}
+				counter++;
+			}
+		}
 		return 0;	// just for the heck of it
 	}
 	
 	private int getRow(int cellId) {
 		// WRITE YOUR LOGIC HERE....................................
-		
+		int counter=0;
+		for(int i=0;i<8;i++) {
+			for(int k=0;k<8;k++) {
+				if(counter==cellId) {
+					return i;
+				}
+				counter++;
+			}
+		}
 		return 0;	// just for the heck of it
 	}
 
@@ -27,8 +43,37 @@ public class GameStrategy {
 		
 		/*
 			WRITE YOUR LOGIC HERE...............................
-
 		*/
+		if(!this.placedQueens[row][col]) {
+			this.placedQueens[row][col]=true;
+			
+			//for columns
+			for(int i=0;i<8;i++) this.placedQueens[row][i] = true;
+			//for rows
+			for(int i=0;i<8;i++) this.placedQueens[i][col] = true;
+			
+			//upper left diagonal
+			for(int i=row-1, j=col-1; i>=0 && j>=0; i--,j--) {
+				this.placedQueens[i][j] = true;
+			}
+			//upper right diagonal
+			for(int i=row-1, j=col+1; i>=0 && j<8; i--,j++) {
+				this.placedQueens[i][j] = true;
+			}
+			//lower left diagonal
+			for(int i=row+1, j=col-1; i<8 && j>=0; i++,j--) {
+				this.placedQueens[i][j] = true;
+			}
+			//lower right diagonal
+			for(int i=row+1, j=col+1; i<8 && j<8; i++,j++) {
+				this.placedQueens[i][j] = true;
+			}
+			
+		}
+		else
+			return false;
+		
+		this.numQueens++;
 		return isValid;
 	}
 }
